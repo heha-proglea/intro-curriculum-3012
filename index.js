e319c238381f2d8658897043a4fd08b63f7799a8
heha-proglea <heha965+proglea@gmail.com>
2018-10-27 03:09:44 +0000

    add: add comments

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 'use strict';
+
+// HTTPモジュールの読み込み
 const http = require('http');
+
+// モジュールhttpを用いて、サーバーを作成
 const server = http.createServer((req, res) => {
-	console.info('[' + new Date() + '] Requested by ' + req.connection.remoteAddress);
-	res.writeHead(200, {
-		'Content-Type': 'text/plain; charset=utf-8'
-	});
-	res.write(req.headers['user-agent']);
-	res.end();
-}).on('error', (e) => {
-	console.error('[' + new Date() + '] Server Error', e);
+    // サーバーにリクエストがあった時に呼び出されるコールバック関数
+
+    // アクセスログを出力
+    console.info('[' + new Date() + '] Requested by ' + req.connection.remoteAddress); // req.cone...ではリクエストが送られたIP情報を取得している
+
+    res.writeHead(200, {
+        'Content-Type': 'text/plain; charset=utf-8'
+    });
+    res.write(req.headers['user-agent']); // req.headers['user-agent']にてユーザーエージェントを取得
+    res.end();
+}).on('error', (e) => { // エラーのハンドリングを行う←「httpサーバーが次のイベントを発行した際に補足するよ」という書き方
+    // 'error'という文字列のイベントが発生した時に呼び出されるコールバック関数
+    console.error('[' + new Date() + '] Server Error', e);
 }).on('clientError', (e) => {
-	console.error('[' + new Date() + '] Client Error', e);
+    // 'clientError'という文字列のイベントが発生した時に呼び出されるコールバック関数
+    console.error('[' + new Date() + '] Client Error', e);
 });
+
+// このHTTPが起動するポートを宣言し、サーバーを起動
 const port = 8000;
 server.listen(port, () => {
-	console.info('[' + new Date() + '] Listening on ' + port);
+    // サーバーを起動時に実行されるコールバック関数
+    // console.log('Listening on ' + port);
+    console.info('[' + new Date() + '] Listening on ' + port)
 });
